33c532f722b7ce6be6fc0c724cda36e91b3a3d1c
yadav.sanjay <yadav.sanjay@naukri.com>
2019-04-27 14:45:03 +0530

    Added one more line in HelloWorld.java file

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		System.out.println("Added one more line to trigger build automatically when there is any commit occured on github");
 	}
 }
